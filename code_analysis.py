c30263571eb4dd983ff8032ad05d657dea77c3a8
Ahmadreza-SY <ahmadreza.saboor.y@gmail.com>
2022-08-05 15:36:49 -0400

    refactor python data collection code

---
+++ code_analysis.py
@@ -0,0 +1,84 @@
+import pickle
+from pathlib import Path
+import pandas as pd
+import jparser
+from tqdm import tqdm
+import json
+from config import Config
+
+
+def create_repaired_tc_call_graphs():
+    repair_info = pd.read_csv(Path(Config.get("output_path")) / "repairs" / "test_repair_info.csv")
+    base_tags = repair_info["base_tag"].unique()
+    for base_tag in tqdm(base_tags, ncols=100, position=0, leave=True, desc="Creating call graphs"):
+        jparser.create_call_graphs(Path(Config.get("output_path")), base_tag)
+
+
+def get_call_graph(_class, method, tag):
+    call_graph_path = Path(Config.get("output_path")) / "releases" / tag / "call_graphs" / _class / f"{method}.json"
+    call_graph = {}
+    with open(call_graph_path) as f:
+        call_graph = json.loads(f.read())
+
+    return call_graph
+
+
+def get_test_file_coverage(_class, method, tag):
+    all_tests = pd.read_csv(Path(Config.get("output_path")) / "releases" / tag / "tests.csv")
+    all_test_files = all_tests["PATH"].values.tolist()
+
+    call_graph = get_call_graph(_class, method, tag)
+
+    return set([n["path"] for n in call_graph["nodes"] if n["path"] not in all_test_files])
+
+
+def get_release_changed_files(base_tag, head_tag):
+    release_patches_path = Path(Config.get("output_path")) / "repairs" / f"{base_tag}...{head_tag}" / "patches.pickle"
+    patches = pickle.load(open(str(release_patches_path), "rb"))
+    return set([patch.path for patch in patches["patches"].modified_files])
+
+
+def create_repaired_tc_change_coverage():
+    repair_info = pd.read_csv(Path(Config.get("output_path")) / "repairs" / "test_repair_info.csv")
+
+    change_coverage = []
+    for _, r in tqdm(
+        repair_info.iterrows(),
+        total=len(repair_info),
+        ncols=100,
+        position=0,
+        leave=True,
+        desc="Creating test change coverage",
+    ):
+        _class, method, base_tag, head_tag = (
+            r["class"],
+            r["method"],
+            r["base_tag"],
+            r["head_tag"],
+        )
+        tc_coverage = get_test_file_coverage(_class, method, base_tag)
+        changed_files = get_release_changed_files(base_tag, head_tag)
+        tc_change_coverage = list(tc_coverage.intersection(changed_files))
+        cov_per = len(tc_change_coverage) / len(tc_coverage)
+        chn_per = len(tc_change_coverage) / len(changed_files)
+        change_coverage.append(
+            {
+                "test": f"{_class}.{method}",
+                "baseTag": base_tag,
+                "headTag": head_tag,
+                "covered_changed_files": tc_change_coverage,
+                "coverage_percentage": f"{cov_per:.2f}",
+                "change_percentage": f"{chn_per:.3f}",
+            }
+        )
+
+    cov_output_file = Path(Config.get("output_path")) / "repairs" / "test_change_coverage.json"
+    with open(cov_output_file, "w") as f:
+        f.write(json.dumps(change_coverage))
+
+    jparser.detect_changed_methods(Config.get("output_path"))
+
+
+def get_test_method_coverage(_class, method, tag):
+    call_graph = get_call_graph(_class, method, tag)
+    return set([n["name"] for n in call_graph["nodes"]])
---
+++ release_analysis.py
@@ -0,0 +1,164 @@
+from unidiff import PatchSet
+import pickle
+from pathlib import Path
+import re
+import pandas as pd
+import github_api as ghapi
+import jparser
+import tarfile
+import shutil
+from config import Config
+import jparser
+
+
+class Release:
+    def __init__(self, name, tag, date, tarball_url):
+        self.name = name
+        self.tag = tag
+        self.date = date
+        self.tarball_url = tarball_url
+
+    def __str__(self) -> str:
+        return self.tag
+
+    def download_source_code(self):
+        release_path = Path(Config.get("output_path")) / "releases" / self.tag
+        release_path.mkdir(parents=True, exist_ok=True)
+        code_path = release_path / "code"
+        if code_path.exists():
+            return code_path
+
+        code_tar_file = release_path / f"{self.tag}.tar.gz"
+        ghapi.download_file(self.tarball_url, code_tar_file)
+        with tarfile.open(str(code_tar_file)) as f:
+            f.extractall(str(release_path / "temp"))
+        temp_code_path = list((release_path / "temp").glob("*"))[0]
+        shutil.move(str(temp_code_path), str(code_path))
+        shutil.rmtree(str(release_path / "temp"))
+        return code_path
+
+    def get_test_out_path(self, _class):
+        return Path(Config.get("output_path")) / "releases" / self.tag / "changed_tests" / _class
+
+    def fetch_and_save_test_code(self, test_path):
+        file_content = ghapi.get_test_file(self.tag, test_path, Config.get("repo"))
+
+        package_name = None
+        matches = re.compile("package (.+);").findall(file_content)
+        if len(matches) == 1:
+            package_name = matches[0]
+
+        full_name = f"{package_name}.{Path(test_path.stem)}"
+        test_out_path = self.get_test_out_path(full_name)
+        test_out_path.mkdir(parents=True, exist_ok=True)
+        test_out_file = test_out_path / test_path.name
+        with open(str(test_out_file), "w") as f:
+            f.write(file_content)
+
+        jparser.extract_test_methods(test_out_file)
+
+        return full_name
+
+
+class ReleasePair:
+    def __init__(self, base, head):
+        self.base = base
+        self.head = head
+        self.patches = None
+        self.test_patches = None
+
+    def __str__(self) -> str:
+        return f"{self.base.tag}...{self.head.tag}"
+
+    def extract_release_repairs(self):
+        self.fetch_patches()
+
+        test_release_info = self.create_test_release_info()
+        test_repair_info = self.create_test_repair_info(test_release_info)
+
+        if len(test_repair_info) > 0:
+            self.save_patches()
+
+        return test_release_info, test_repair_info
+
+    def fetch_patches(self):
+        base_code_path = self.base.download_source_code()
+        base_tests = jparser.find_test_classes(base_code_path)
+        head_code_path = self.head.download_source_code()
+        head_tests = jparser.find_test_classes(head_code_path)
+        release_test_path_set = set(base_tests["PATH"].values.tolist() + head_tests["PATH"].values.tolist())
+        diff = ghapi.get_diff(self, Config.get("repo"))
+        patches = PatchSet(diff)
+        test_patches = [patch for patch in patches.modified_files if patch.path in release_test_path_set]
+        self.patches = patches
+        self.test_patches = test_patches
+
+    def save_patches(self):
+        repairs_path = Path(Config.get("output_path")) / "repairs" / f"{self.base.tag}...{self.head.tag}"
+        repairs_path.mkdir(parents=True, exist_ok=True)
+        pickle.dump(
+            {"patches": self.patches, "test_patches": self.test_patches},
+            open(str(repairs_path / "patches.pickle"), "wb"),
+        )
+
+    def create_test_release_info(self):
+        def update_test_release_info(_class, path, release):
+            test_release_info["class"].append(_class)
+            test_release_info["path"].append(path)
+            test_release_info["release_tag"].append(release.tag)
+            test_release_info["release_date"].append(release.date)
+
+        test_release_info = {
+            "class": [],
+            "path": [],
+            "release_tag": [],
+            "release_date": [],
+        }
+        for test_patch in self.test_patches:
+            test_path = Path(test_patch.path)
+            full_name = self.head.fetch_and_save_test_code(test_path)
+            update_test_release_info(full_name, str(test_path), self.head)
+            if test_patch.is_rename:
+                test_path = test_patch.source_file
+                if test_path.startswith("a/") or test_path.startswith("b/"):
+                    test_path = test_path[2:]
+                test_path = Path(test_path)
+            full_name = self.base.fetch_and_save_test_code(test_path)
+            update_test_release_info(full_name, str(test_path), self.base)
+
+        return pd.DataFrame(test_release_info)
+
+    def find_changed_methods(self, _class):
+        base_path = self.base.get_test_out_path(_class)
+        head_path = self.head.get_test_out_path(_class)
+
+        changed_methods = []
+        for bm_path in (base_path / "methods").glob("*"):
+            hm_path = head_path / "methods" / bm_path.name
+            if hm_path.exists():
+                with open(str(bm_path)) as f:
+                    bm_code = f.read()
+                with open(str(hm_path)) as f:
+                    hm_code = f.read()
+                if bm_code != hm_code:
+                    changed_methods.append(bm_path.name)
+        return changed_methods
+
+    def create_test_repair_info(self, test_release_info):
+        path_to_class = dict(
+            zip(
+                test_release_info["path"].values.tolist(),
+                test_release_info["class"].values.tolist(),
+            )
+        )
+        test_repair_info = {"class": [], "method": [], "base_tag": [], "head_tag": []}
+        for test_patch in self.test_patches:
+            test_path = Path(test_patch.path)
+            _class = path_to_class[str(test_path)]
+            changed_methods = self.find_changed_methods(_class)
+            for method in changed_methods:
+                test_repair_info["class"].append(_class)
+                test_repair_info["method"].append(method)
+                test_repair_info["base_tag"].append(self.base.tag)
+                test_repair_info["head_tag"].append(self.head.tag)
+        return pd.DataFrame(test_repair_info)
