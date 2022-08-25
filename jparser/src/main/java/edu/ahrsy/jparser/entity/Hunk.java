a3083d72749964cf693184db6abd9ff905c3ff95
Ahmadreza-SY <ahmadreza.saboor.y@gmail.com>
2022-08-25 15:00:47 -0400

    Fix method change detection issues: removed comments and whitespaces from changes, fixed incomplete changes (annotation newlines), and added method signature change detection

--- jparser/src/main/java/edu/ahrsy/jparser/entity/Hunk.java
+++ jparser/src/main/java/edu/ahrsy/jparser/entity/Hunk.java
@@ -1,6 +1,7 @@
 package edu.ahrsy.jparser.entity;
 
 import com.github.difflib.patch.AbstractDelta;
+import com.github.difflib.patch.Chunk;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,42 +11,36 @@ public class Hunk {
   public List<LineChange> targetChanges;
   public ChangeType type;
 
+  private static List<LineChange> getLines(Chunk<String> chunk, ChangeType type) {
+    return chunk.getLines()
+            .stream()
+            .map(l -> new LineChange(l, type))
+            .filter(l -> !l.line.isBlank())
+            .collect(Collectors.toList());
+  }
+
   public static Hunk from(AbstractDelta<String> delta) {
     var hunk = new Hunk();
     switch (delta.getType()) {
       case CHANGE:
         hunk.type = ChangeType.MODIFY;
-        hunk.sourceChanges = delta.getSource()
-                .getLines()
-                .stream()
-                .map(l -> new LineChange(l, ChangeType.DELETE))
-                .collect(Collectors.toList());
-        hunk.targetChanges = delta.getTarget()
-                .getLines()
-                .stream()
-                .map(l -> new LineChange(l, ChangeType.ADD))
-                .collect(Collectors.toList());
+        hunk.sourceChanges = getLines(delta.getSource(), ChangeType.DELETE);
+        hunk.targetChanges = getLines(delta.getTarget(), ChangeType.ADD);
         break;
       case INSERT:
         hunk.type = ChangeType.ADD;
         if (!delta.getSource().getLines().isEmpty())
           throw new RuntimeException("Source of INSERT delta is not empty! " + delta);
-        hunk.targetChanges = delta.getTarget()
-                .getLines()
-                .stream()
-                .map(l -> new LineChange(l, ChangeType.ADD))
-                .collect(Collectors.toList());
+        hunk.targetChanges = getLines(delta.getTarget(), ChangeType.ADD);
         break;
       case DELETE:
         hunk.type = ChangeType.DELETE;
         if (!delta.getTarget().getLines().isEmpty())
           throw new RuntimeException("Target of DELETE delta is not empty! " + delta);
-        hunk.sourceChanges = delta.getSource()
-                .getLines()
-                .stream()
-                .map(l -> new LineChange(l, ChangeType.DELETE))
-                .collect(Collectors.toList());
+        hunk.sourceChanges = getLines(delta.getSource(), ChangeType.DELETE);
         break;
+      case EQUAL:
+        throw new RuntimeException("No change detected!");
     }
     return hunk;
   }
