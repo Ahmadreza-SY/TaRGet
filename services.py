2ac7e7737ace0afbc98f72e84e6efd95b75c0cf1
Ahmadreza-SY <ahmadreza.saboor.y@gmail.com>
2022-10-18 12:42:33 -0400

    fix issue in querying depths from call graphs

--- services.py
+++ services.py
@@ -6,7 +6,7 @@ import json
 from config import Config
 from release_analysis import Release, ReleasePair
 from code_analysis import create_repaired_tc_call_graphs, create_repaired_tc_change_coverage, get_test_method_coverage
-
+import copy
 
 class Service:
     @staticmethod
@@ -95,8 +95,9 @@ class Service:
             for change in changed_methods.get(base_tag, []):
                 found_coverage_i = [i for i, mc in enumerate(method_coverage) if mc["name"] == change["name"]]
                 if len(found_coverage_i) > 0 and len(change["hunks"]) > 0:
-                    change["depth"] = method_coverage[found_coverage_i[0]]["depth"]
-                    covered_changes.append(change)
+                    _change = copy.deepcopy(change)
+                    _change["depth"] = method_coverage[found_coverage_i[0]]["depth"]
+                    covered_changes.append(_change)
             repair_changes = repair_changes_map[f"{base_tag}-{head_tag}-{name}"]
 
             dataset.append(
