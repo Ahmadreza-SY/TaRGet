5a5b85f20a52511e0c195cbf9ecfd85e685911c8
Darren Holden <darren.a.holden@outlook.com>
2022-11-16 19:50:55 -0500

    More Cleanup

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ class Config:
         "repo": None,
         "output_path": None,
         "jparser_path": "assets/jparser.jar",
-        "gh_api_token": None,
         "gh_api_base_url": "https://api.github.com",
         "gh_raw_base_url": "https://raw.githubusercontent.com",
         "gh_cache_path": "./api_cache",
