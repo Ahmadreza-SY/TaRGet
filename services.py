3c4a3c7ce9c14fac869790d9cc361c6af42a035e
Ahmadreza-SY <ahmadreza.saboor.y@gmail.com>
2022-11-24 11:56:16 -0500

    add refactoring types and clean services.py code

--- services.py
+++ services.py
@@ -50,13 +50,84 @@ class Service:
         return code, body_code
 
     @staticmethod
-    def create_dataset():
+    def create_test_repair(repair_info, features):
+        _class, method, base_tag, head_tag = (
+            repair_info["class"],
+            repair_info["method"],
+            repair_info["base_tag"],
+            repair_info["head_tag"],
+        )
+        test_paths, changed_methods, refactored_methods, refactor_types, repair_changes = (
+            features["test_paths"],
+            features["changed_methods"],
+            features["refactored_methods"],
+            features["refactor_types"],
+            features["repair_changes"],
+        )
+        name = f"{_class}.{method}"
+        path = test_paths[base_tag][_class]
+        before_repair, before_repair_body = Service.get_test_code(base_tag, _class, method)
+        after_repair, after_repair_body = Service.get_test_code(head_tag, _class, method)
+        method_coverage = get_test_method_coverage(_class, method, base_tag)
+        if method_coverage is None:
+            print(f"No call graph found for {name} ! Skipping ...")
+            return None
+
+        tag_pair = (base_tag, head_tag)
+        covered_changes = []
+        for change in changed_methods.get(base_tag, []):
+            found_coverage_i = [i for i, mc in enumerate(method_coverage) if mc["name"] == change["name"]]
+            if len(found_coverage_i) > 0 and len(change["hunks"]) > 0:
+                _change = copy.deepcopy(change)
+                _change["depth"] = method_coverage[found_coverage_i[0]]["depth"]
+                _change["refactor"] = _change["name"] in refactored_methods[tag_pair]
+                _change["refactor_types"] = (
+                    None if not _change["refactor"] else refactor_types[tag_pair][_change["name"]]
+                )
+                covered_changes.append(_change)
+        repair_changes = repair_changes[f"{base_tag}-{head_tag}-{name}"]
+
+        test_refactored = name in refactored_methods[tag_pair]
+        return {
+            "name": name,
+            "path": path,
+            "base_tag": base_tag,
+            "head_tag": head_tag,
+            "before_repair": before_repair,
+            "before_repair_body": before_repair_body,
+            "after_repair": after_repair,
+            "after_repair_body": after_repair_body,
+            "covered_changes": covered_changes,
+            "repair_changes": repair_changes,
+            "refactor": test_refactored,
+            "refactor_types": None if not test_refactored else refactor_types[tag_pair][name],
+        }
+
+    @staticmethod
+    def compute_features():
         create_repaired_tc_call_graphs()
         create_repaired_tc_change_coverage()
         refactorings = mine_method_refactorings()
-        refactorings = {tag_pair: set([r["method"] for r in mrefs]) for tag_pair, mrefs in refactorings.items()}
+
+        refactored_methods = {tag_pair: set([r["method"] for r in mrefs]) for tag_pair, mrefs in refactorings.items()}
+        refactor_types = {}
+        for tag_pair, mrefs in refactorings.items():
+            tag_refactor_types = {}
+            for mref in mrefs:
+                method_refactor_types = []
+                for c in mref["refactoringCommits"]:
+                    method_refactor_types.extend(c["refactorings"])
+                tag_refactor_types[mref["method"]] = method_refactor_types
+            refactor_types[tag_pair] = tag_refactor_types
 
         repair_info = pd.read_csv(Path(Config.get("output_path")) / "repairs" / "repaired_test_methods.csv")
+        test_paths = {}
+        for _, r in repair_info.iterrows():
+            base_tag = r["base_tag"]
+            if base_tag not in test_paths:
+                tests = pd.read_csv(Path(Config.get("output_path")) / "tags" / base_tag / "tests.csv")
+                test_paths[base_tag] = dict(zip(tests["NAME"].values.tolist(), tests["PATH"].values.tolist()))
+
         full_changed_methods = json.loads(
             (Path(Config.get("output_path")) / "repairs" / "test_coverage_changed_methods.json").read_text()
         )
@@ -65,10 +136,23 @@ class Service:
         test_repair_changes = json.loads(
             (Path(Config.get("output_path")) / "repairs" / "test_repair_changes.json").read_text()
         )
-        repair_changes_map = {f"{ch['baseTag']}-{ch['headTag']}-{ch['name']}": ch["hunks"] for ch in test_repair_changes}
+        repair_changes = {f"{ch['baseTag']}-{ch['headTag']}-{ch['name']}": ch["hunks"] for ch in test_repair_changes}
+
+        return {
+            "repair_info": repair_info,
+            "refactored_methods": refactored_methods,
+            "refactor_types": refactor_types,
+            "test_paths": test_paths,
+            "changed_methods": changed_methods,
+            "repair_changes": repair_changes,
+        }
+
+    @staticmethod
+    def create_dataset():
+        features = Service.compute_features()
 
         dataset = []
-        test_paths = {}
+        repair_info = features["repair_info"]
         for _, r in tqdm(
             repair_info.iterrows(),
             total=len(repair_info),
@@ -77,50 +161,9 @@ class Service:
             leave=True,
             desc="Creating dataset",
         ):
-            _class, method, base_tag, head_tag = (
-                r["class"],
-                r["method"],
-                r["base_tag"],
-                r["head_tag"],
-            )
-            name = f"{_class}.{method}"
-            if base_tag not in test_paths:
-                tests = pd.read_csv(Path(Config.get("output_path")) / "tags" / base_tag / "tests.csv")
-                test_paths[base_tag] = dict(zip(tests["NAME"].values.tolist(), tests["PATH"].values.tolist()))
-            path = test_paths[base_tag][_class]
-            before_repair, before_repair_body = Service.get_test_code(base_tag, _class, method)
-            after_repair, after_repair_body = Service.get_test_code(head_tag, _class, method)
-            method_coverage = get_test_method_coverage(_class, method, base_tag)
-            if method_coverage is None:
-                print(f"No call graph found for {name} ! Skipping ...")
-                continue
-
-            refactored_methods = refactorings[(base_tag, head_tag)]
-            covered_changes = []
-            for change in changed_methods.get(base_tag, []):
-                found_coverage_i = [i for i, mc in enumerate(method_coverage) if mc["name"] == change["name"]]
-                if len(found_coverage_i) > 0 and len(change["hunks"]) > 0:
-                    _change = copy.deepcopy(change)
-                    _change["depth"] = method_coverage[found_coverage_i[0]]["depth"]
-                    _change["refactor"] = _change["name"] in refactored_methods
-                    covered_changes.append(_change)
-            repair_changes = repair_changes_map[f"{base_tag}-{head_tag}-{name}"]
-
-            dataset.append(
-                {
-                    "name": name,
-                    "path": path,
-                    "base_tag": base_tag,
-                    "head_tag": head_tag,
-                    "before_repair": before_repair,
-                    "before_repair_body": before_repair_body,
-                    "after_repair": after_repair,
-                    "after_repair_body": after_repair_body,
-                    "covered_changes": covered_changes,
-                    "repair_changes": repair_changes,
-                    "refactor": name in refactored_methods,
-                }
-            )
+            repair = Service.create_test_repair(r, features)
+            if repair is not None:
+                dataset.append(repair)
 
         (Path(Config.get("output_path")) / "dataset.json").write_text(
             json.dumps(dataset, indent=2, sort_keys=False), encoding="utf-8"
