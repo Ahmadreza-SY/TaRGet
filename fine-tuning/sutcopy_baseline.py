e638d1b5cef06e54b586866b134390121242edcb
Ahmadreza-SY <ahmadreza.saboor.y@gmail.com>
2023-11-16 14:10:52 -0500

    Sort diff pairs in SUTCopy by size Refactor code Modify output format

--- fine-tuning/sutcopy_baseline.py
+++ fine-tuning/sutcopy_baseline.py
@@ -6,9 +6,13 @@ from tqdm import tqdm
 from encoders.repositories.changeRepo import ChangeRepository
 from encoders.abstractEncoder import AbstractDataEncoder
 import argparse
+import pandas as pd
+from eval import compute_scores
+from datetime import datetime
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--test_file", required=True, type=str)
     parser.add_argument("-d", "--dataset_dir", required=True, type=str)
     parser.add_argument("-o", "--output_dir", required=True, type=str)
     args = parser.parse_args()
@@ -16,17 +20,17 @@ if __name__=="__main__":
     ade = AbstractDataEncoder(None)
     change_repo = ChangeRepository(args=args)
 
-    with open(f'{args.output_dir}/splits/test.json', 'r') as f:
+    with open(args.test_file, 'r') as f:
         test_set = json.load(f)
 
+    start = datetime.now()
     preds = []
     count = 0
-    for t in tqdm(test_set):
+    for t in tqdm(test_set, desc="Applying SUTCopy to test set"):
         broken = ade.get_broken_code(t)
         target = ade.get_repaired_code(t)
-        prediction = ""
 
-        sut_changes = change_repo.get_commit_changes(t["ID"].split(":")[0], t["aCommit"])
+        sut_changes = change_repo.get_commit_changes(t['project'], t["aCommit"])
 
         all_diff_pairs = []
         for c in sut_changes:
@@ -53,6 +57,7 @@ if __name__=="__main__":
 
                         i += 1
 
+        all_diff_pairs = sorted(all_diff_pairs, key=lambda p: len(p[0]), reverse=True)
         for d in all_diff_pairs:
             if broken.count(d[0]) == 1:
                 broken = broken.replace(d[0], d[1])
@@ -61,7 +66,13 @@ if __name__=="__main__":
                 "target": target,
                 "preds": [broken]})
 
-    Path(f'{args.output_dir}/sutcopy/').mkdir(parents=True, exist_ok=True)
+    args.output_dir = Path(args.output_dir)
+    args.output_dir.mkdir(parents=True, exist_ok=True)
 
-    with open(f'{args.output_dir}/sutcopy/test_predictions.json', 'w') as f:
-        json.dump(preds, f, indent=4)
+    pred_df = pd.DataFrame(preds)
+    bleu_score, code_bleu_score, em = compute_scores(pred_df)
+    print(f"* BLEU: {bleu_score} ; CodeBLEU: {code_bleu_score} ; EM: {em} ; Eval took: {datetime.now() - start}")
+    stats = {'testset_size': len(test_set), 'test_results': {"bleu": bleu_score, "code_bleu": code_bleu_score, "em": em}}
+    
+    (args.output_dir / 'stats.json').write_text(json.dumps(stats, indent=2, sort_keys=False))
+    pred_df.to_json(args.output_dir / 'test_predictions.json', orient="records", indent=2)
